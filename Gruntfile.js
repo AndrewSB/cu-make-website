25b6a79de402c11fa1cd5c07870c7ff11bb41ea9
Andrew Breckenridge <asbreckenridge@me.com>
2015-03-09 01:33:22 -0500

    add navbar

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function(grunt) {
         files: {
           'css/app.css': 'scss/app.scss',
           'css/custom.css': 'scss/custom.scss',
-          'css/media-queries.css': 'scss/media-queries.scss'
+          'css/media-queries.css': 'scss/media-queries.scss',
+          'css/navbar.css': 'scss/navbar.scss'
         }
       }
     },
