48b098136b71945bfb5d1f9f108cd803c04e1934
Andrew Breckenridge <asbreckenridge@me.com>
2015-03-10 22:41:00 -0500

    work on CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,8 @@ module.exports = function(grunt) {
           'css/app.css': 'scss/app.scss',
           'css/custom.css': 'scss/custom.scss',
           'css/media-queries.css': 'scss/media-queries.scss',
-          'css/navigation.css': 'scss/navigation.scss'
+          'css/navigation.css': 'scss/navigation.scss',
+          'css/background-video.css': 'scss/background-video.scss'
         }
       }
     },
