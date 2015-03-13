eb6d5f5b632bb9e3169c6f746b479426a95ea702
Noah Laferty <noah.lafferty@gmail.com>
2015-03-12 20:06:38 -0500

    styling for most sections, nav bar

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
-   $('.header').smint();
+   $('.top-bar a').on('click', function() {
+
+        var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 70;
+
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 500);
+
+        return false; 
+
+    })
+
+
+    $(window).scroll(function() {
+        var windscroll = $(window).scrollTop();
+        var navFix = $('.about').position().top - 70;
+        if (windscroll >= navFix) {
+            $('.top-bar').addClass('fixed');
+            $('.wrapper > section').each(function(i) {
+                if ($(this).position().top <= windscroll - 70) {
+                    $('nav a.active').removeClass('active');
+                    $('nav a').eq(i).addClass('active');
+                }
+            });
+
+        } else {
+            $('.top-bar').removeClass('fixed');
+            $('.top-bar a.active').removeClass('active');
+        }
+
+    }).scroll();
 });
